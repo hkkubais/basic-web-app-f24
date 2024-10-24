0b5ead3b9a682ec5103e35323c7f79f096510a58
Haya <hkkubais@andrew.cmu.edu>
2024-10-24 16:46:08 +0300

    m

--- utils/QueryProcessor.tsx
+++ utils/QueryProcessor.tsx
@@ -1,4 +1,22 @@
 export default function QueryProcessor(query: string): string {
+
+  if (query.toLowerCase().includes("shakespeare")) {
+    return (
+      "William Shakespeare (26 April 1564 - 23 April 1616) was an " +
+      "English poet, playwright, and actor, widely regarded as the greatest " +
+      "writer in the English language and the world's pre-eminent dramatist."
+    );
+  }
+
+  if (query.toLowerCase().includes("andrew id")) {
+    //TODO add your Andrew ID below
+    //TODO update the corresponding test case in _tests_
+    return ( "hkkubais" );
+  }
+
+  if (query.includes("What is your name?")) {
+    return ("hkkubais-313");
+  }
   if (query.includes("What is") && query.toLowerCase().includes("plus")) {
     const numbers = query.match(/\d+/g)?.map(Number);
     if (numbers && numbers.length === 2) {
@@ -18,24 +36,6 @@ export default function QueryProcessor(query: string): string {
     if (numbers && numbers.length > 0) {
       return Math.max(...numbers).toString();
     }
-  }
-
-  if (query.toLowerCase().includes("shakespeare")) {
-    return (
-      "William Shakespeare (26 April 1564 - 23 April 1616) was an " +
-      "English poet, playwright, and actor, widely regarded as the greatest " +
-      "writer in the English language and the world's pre-eminent dramatist."
-    );
-  }
-
-  if (query.toLowerCase().includes("andrew id")) {
-    //TODO add your Andrew ID below
-    //TODO update the corresponding test case in _tests_
-    return ( "hkkubais" );
-  }
-
-  if (query.includes("What is your name?")) {
-    return ("hkkubais-313");
   }
   return "";
   
